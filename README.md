4e04c04987730dbd47b7a2774576ffced09729e5
Bear <u9206148@gmail.com>
2014-11-07 22:33:05 +0800

    generate ssledge bypass domain to foxyproxy json config

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ tools
 =====
 
 some useful tools for myself
+
+###wildcard_bypass_domain
+######remove EOF
+$ perl -pe 'chomp if eof' ByPass_domain.txt  > ByPass_domain_without_eof.txt
+######generate FoxyProxy config
+$ ./cook_to_foxyproxy_json.pl > foxyProxy_bypass.json
---
+++ wildcard_bypass_domain/cook_to_foxyproxy_json.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+open(my $fh, "ByPass_domain_without_eof.txt") or die "no file";
+my $config_template = '{"enabled":true,"name":"domain","pattern":"domain","isRegEx":false,"caseSensitive":false,"blackList":true,"multiLine":false},';
+my @arr;
+my $temp;
+my $all_config = "";
+while ( my $row =  <$fh> ){
+	@arr = split(",",$row);	
+	for my $domain (@arr){
+		$temp = $config_template;
+		$temp =~ s/domain/$domain/g;
+		$all_config .= $temp;
+	}
+}
+$all_config =~ s/,$//;	# remove last "."
+print "{\"patterns\":[$all_config]}";
+print "\n";
